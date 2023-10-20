f0c28dcd2f4fe7140d86d4d82513409fc69399ec
Roy Cornelissen <roy_cornelissen@hotmail.com>
2023-10-20 13:58:27 +0200

    👨‍🔧 Fix unittests execution

--- unittests/UnitTest1.cs
+++ unittests/UnitTest1.cs
@@ -23,5 +23,12 @@ namespace unittests
         {
             Assert.IsTrue(true);
         }
+
+        [TestMethod]
+        public void Dummy_test()
+        {
+            var c = new GloboTicket.Catalog.Artist(Guid.NewGuid(), "Rolling Stones", "Rock'n'Roll");
+            Assert.AreEqual(c.Name, "Rolling Stones");
+        }
     }
 }
